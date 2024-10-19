2cfc7d8751d0f4a8b0d29b965ec04733adf1b13e
Sowon Kim <sso626262@naver.com>
2024-10-19 21:44:12 +0900

    Add container-presentational folder
    custom hook 실습 내용 추가

---
+++ container-presentational/src/App.jsx
@@ -0,0 +1,20 @@
+import React from "react";
+import "./styles.css";
+
+import DogImages from "./DogImages";
+
+function App() {
+  return (
+    <div className="App">
+      <h1>
+        Browse Dog Images{" "}
+        <span role="img" aria-label="emoji">
+          🐕
+        </span>
+      </h1>
+      <DogImages />
+    </div>
+  )
+}
+
+export default App
